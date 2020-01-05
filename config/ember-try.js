ce3a5a8e4b66811a51dab64e21b3579144c8d8b2
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2020-01-05 16:40:29 +0300

    Skip 3.8 LTS tests

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,14 +6,14 @@ module.exports = async function () {
   return {
     useYarn   : true,
     scenarios : [
-      // {
-      //   name : 'ember-lts-3.8',
-      //   npm  : {
-      //     devDependencies : {
-      //       'ember-source' : '~3.8.0',
-      //     },
-      //   },
-      // },
+      {
+        name : 'ember-lts-3.8',
+        npm  : {
+          devDependencies : {
+            'ember-source' : '~3.8.0',
+          },
+        },
+      },
       {
         name : 'ember-lts-3.12',
         npm  : {
